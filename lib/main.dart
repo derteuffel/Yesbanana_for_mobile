265218b76750e381de71fa4d48c4c00272689a52
Neword <newordn@gmail.com>
2019-09-01 07:43:37 +0100

    Just fixed bottom overflowed pixel on login, and removing the debuging bar

--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Bottom Navigation',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.cyan,
         ),
